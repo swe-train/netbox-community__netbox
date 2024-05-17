1253d9188fdcf39d7995fab9d172b99d959d69f3
Arthur <worldnomad@gmail.com>
2024-05-17 11:17:40 -0700

    16145 update doc string

--- netbox/extras/api/views.py
+++ netbox/extras/api/views.py
@@ -241,7 +241,7 @@ class ScriptViewSet(ModelViewSet):
 
     def post(self, request, pk):
         """
-        Run a Script identified by the name and return the pending Job as the result
+        Run a Script identified by the name or pk and return the pending Job as the result
         """
 
         if not request.user.has_perm('extras.run_script'):
